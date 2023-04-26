76810e3eb3768dcf16e1ccaf383218dcd56e1736
keanu-j-a <107134356+keanu-j-a@users.noreply.github.com>
2023-04-26 16:29:44 -0400

    Update CNN_Models.py

--- CNN_Models.py
+++ CNN_Models.py
@@ -1,30 +1,4 @@
 
-"""
-
-# Working Model from previous experiments
-# Cats and dogs raw model
-model = Sequential()
-
-model.add(Conv2D(64, (3, 3), input_shape=X.shape[1:]))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-model.add(Conv2D(64, (3, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
-
-model.add(Dense(64))
-
-model.add(Dense(1))
-model.add(Activation('sigmoid'))
-
-model.compile(loss='binary_crossentropy',
-              optimizer='adam',
-              metrics=['accuracy'])
-
-"""
 
 # Newest model based on previous data.
 model = Sequential()
